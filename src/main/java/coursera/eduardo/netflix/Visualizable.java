4ff58ac2b8106237a40753aac69ae760b4759199
Eduardo <eduardo_lr@ciencias.unam.mx>
2021-09-15 12:48:52 -0500

    Visualizable documentada. Todo esta documentado

--- src/main/java/coursera/eduardo/netflix/Visualizable.java
+++ src/main/java/coursera/eduardo/netflix/Visualizable.java
@@ -1,10 +1,25 @@
 package coursera.eduardo.netflix;
 
+/**
+ * <p> Interfaz para modelar el comportamiento
+ * de un programa visualizable. </p>
+ */
 interface Visualizable {
-	
+
+	/**
+	 * Marca en visto el visualizable.
+	 */
 	public void marcarVisto();
 
+	/**
+	 * Regresa el estado de visualización.
+	 * @return el estado de visualización.
+	 */
     public boolean esVisto();
 
+	/**
+	 * Regresa el tiempo que ha sido visto el visualizable.
+	 * @return el tiempo que ha sido visto el visualizable.
+	 */
     public String tiempoVisto();
 }
