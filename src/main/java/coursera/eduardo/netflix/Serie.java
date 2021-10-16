57dea7d96ab010b305fa19c5d3df943c56b65d99
Eduardo Lara Ramírez <54120988+eduardo-lr@users.noreply.github.com>
2021-10-16 18:41:27 -0500

    Espacios corregidos en Serie

--- src/main/java/coursera/eduardo/netflix/Serie.java
+++ src/main/java/coursera/eduardo/netflix/Serie.java
@@ -6,14 +6,14 @@ package coursera.eduardo.netflix;
 class Serie extends Programa {
 
 	/** El número de temporadas de la serie. */
-    public int temporadas;
+    	public int temporadas;
 
 	/** 
 	 * Constructor sin parámetros. 
 	 */
-    public Serie() {
-        temporadas = 1; 
-    }
+    	public Serie() {
+        	temporadas = 1; 
+    	}
 
 	/**
 	 * Construye una serie a partir de
@@ -21,10 +21,10 @@ class Serie extends Programa {
 	 * @param titulo el titulo de la serie.
 	 * @param creador el creador de la serie.
 	 */
-    public Serie(String titulo, String creador) {
-        super(titulo, creador);
-        this.temporadas = 1;
-    }
+    	public Serie(String titulo, String creador) {
+        	super(titulo, creador);
+        	this.temporadas = 1;
+    	}
 
 	/**
 	 * Construye una serie a partir de todos
@@ -35,43 +35,43 @@ class Serie extends Programa {
 	 * @param creador el creador de la serie.
 	 * @param duracion la duración de la serie, en segundos.
 	 */
-    public Serie(String titulo, int temporadas, String genero, 
+    	public Serie(String titulo, int temporadas, String genero, 
                                 String creador, double duracion) {
-        this.titulo = titulo;
-        this.temporadas = temporadas;
-        this.genero = genero;
-        this.creador = creador;
-        this.duracion = duracion;
-    }
+        	this.titulo = titulo;
+        	this.temporadas = temporadas;
+        	this.genero = genero;
+        	this.creador = creador;
+        	this.duracion = duracion;
+    	}
 
 	/**
 	 * Regresa el número de temporadas de la serie.
 	 * @return el número de temporadas de la serie.
 	 */
-    public int getTemporadas() {
-        return temporadas;
-    }
+    	public int getTemporadas() {
+        	return temporadas;
+    	}
 
 	/**
 	 * Asigna a la serie el número de temporadas dado.
 	 * @param temporadas el número de temporadas a agregar.
 	 */
-    public void setTemporadas(int temporadas) {
-        this.temporadas = temporadas;
-    }
+    	public void setTemporadas(int temporadas) {
+        	this.temporadas = temporadas;
+    	}
 
 	/**
 	 * Regresa una representación en cadena de la serie.
 	 * @return una representación en cadena de la serie.
 	 */
-    @Override public String toString() {
+    	@Override public String toString() {
 		return (String.format("Titulo               : %s\n" +
-                              "No. de temporadas    : %d\n" +
-                              "Visto                : %s\n" +
-                              "Genero               : %s\n" + 
-							  "Creador              : %s\n" +
-							  "Duracion             : %.3f\n",
-                              titulo, temporadas, (visto ? "Sí" : "No"), 
-														genero, creador, duracion));
+                              	      "No. de temporadas    : %d\n" +
+                              	      "Visto                : %s\n" +
+                              	      "Genero               : %s\n" + 
+				      "Creador              : %s\n" +
+				      "Duracion             : %.3f\n",
+                              			titulo, temporadas, (visto ? "Sí" : "No"), 
+								genero, creador, duracion));
 	}
 }
